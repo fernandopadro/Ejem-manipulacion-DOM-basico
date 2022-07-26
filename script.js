cdc1af17e2042f1180478a50ea31e5371936109a
Feru <fernandopahu.92@gmail.com>
2022-07-26 18:04:27 +0900

    agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,14 @@
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
+const parrafito = document.querySelector(".parrafito");
+const pid = document.querySelector("#pid");
+const input = document.querySelector("input");
+
+
+
+h1.innerText = "Fernando es un chingon"
+
+function btnOnClick() {
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
